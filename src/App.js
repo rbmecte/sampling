5980b85a53f55d57dde13320aa7927c2e830b512
peggy.song <peggy.song@iress.com>
2020-08-13 02:53:32 +1000

    sampling app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Button, Form} from "react-bootstrap";
 import './App.css';
+import {generateNumber} from "./util/Util";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sum: 0,
+            count: 0,
+            samples: []
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = Number(target.value);
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    onClick = () => {
+        this.setState({
+            samples: generateNumber(this.state.sum, this.state.count)
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Form className="sample-generator">
+                    <label>
+                        Sum
+                        <input
+                            name="sum"
+                            type="number"
+                            value={Number(this.state.sum).toString()}
+                            min="0"
+                            onChange={this.handleInputChange}/>
+                    </label>
+                    <br/>
+                    <label>
+                        Count
+                        <input
+                            name="count"
+                            type="number"
+                            value={Number(this.state.count).toString()}
+                            max={this.state.sum}
+                            min="0"
+                            onChange={this.handleInputChange}/>
+                    </label>
+                    <Button name="generate" onClick={() => this.onClick()}>
+                        Generate
+                    </Button>
+                </Form>
+                <div className="samples">
+                    <p>
+                        {this.state.samples.join(", ")}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
